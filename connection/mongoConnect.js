84f7b01c97b2518e7020bf38cbb7b6f4970693fc
Zack Bello <obomheire@gmail.com>
2022-09-24 02:45:41 +0100

    Refactore mongouri

--- connection/mongoConnect.js
+++ connection/mongoConnect.js
@@ -2,14 +2,15 @@ import mongoose from 'mongoose'
 
 
 const connect = () => {
-    mongoose.connect(process.env.CONNECTION_URL, {
-        useNewUrlParser: true, 
-        useUnifiedTopology: true 
-       })
-     .then(() =>{
-        console.log('Connected to Database...')
-     })
-     .catch((error) => console.log(`${error} Could not connect to DB`));
+    mongoose
+      .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("Connected to Database...");
+      })
+      .catch((error) => console.log(`${error} Could not connect to DB`));
 }
 
 export default connect
